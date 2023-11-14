1761fabf7c2c1b638fb2dab6d71585803f397a0d
bian <bute77889@gmail.com>
2023-11-14 11:23:18 +0800

    加入專案檔案。

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace MSIT153.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
